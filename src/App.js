2201099083cebe9a3c0d7c1eea496265f36a7cee
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-14 13:53:34 +0600

    navbar creating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/HomePage/Home/Home";
+import NavMenu from "./Pages/NavMenu/NavMenu";
+import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Reviews from "./Pages/Reviews/Reviews";
+import Contact from "./Pages/Contact/Contact";
+import LogIn from "./Pages/LogIn/LogIn";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <NavMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reviews = () => {
+  return (
+    <div>
+      <h2>review</h2>
+    </div>
+  );
+};
+
+export default Reviews;
